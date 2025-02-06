192df77e8b0efe96e22545fef8c14f950006e264
Émile Le Sage <emile.lesage8@gmail.com>
2025-02-06 14:38:41 -0500

    New Clock Sync logic + Prompt to reschedule script on next reboot if there was a problem

--- AutomateUpdates.ps1
+++ AutomateUpdates.ps1
@@ -5,7 +5,6 @@ $RebootCountRegistryPath = "HKLM:\Software\AutomateUpdates"
 $RunOnceRegistryPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
 $MaxReboots = 5
 
-
 function Log-Message {
     param (
         [string]$Message,
@@ -15,9 +14,19 @@ function Log-Message {
     Add-Content -Path $ScriptLogFile -Value "[$Timestamp] [$Level] $Message"
 }
 
-function Cleanup-Script {
+function Get-WindowsUpdateLog {
     Log-Message "Generating Windows Update log File. ($WindowsUpdateLog)"
     Get-WindowsUpdateLog -LogPath $WindowsUpdateLog
+}
+
+# Function that generates Windows Update log file and removes registry key and script file
+function Cleanup-Script {
+    param (
+        [bool]$GenerateLog = $true
+    )
+    if ($GenerateLog) {
+        Get-WindowsUpdateLog
+    }
     Log-Message "Deleting registry key. ($RebootCountRegistryPath)"
     Remove-Item -Path $RebootCountRegistryPath -Recurse -Force
     Log-Message "Deleting script file. ($ScriptPath)"
@@ -26,6 +35,20 @@ function Cleanup-Script {
     Exit
 }
 
+# Function that reschedules the script to run after reboot
+function Reschedule-Script {
+    param (
+        [bool]$ForceReboot = $true
+    )
+    Log-Message "Rescheduling script to run after reboot."
+    Set-ItemProperty -Path $RunOnceRegistryPath -Name "AutomateUpdates" -Value "Powershell.exe -ExecutionPolicy Bypass -File `"$ScriptPath`""
+    if ($ForceReboot) {
+        Log-Message "Rebooting the system to continue script execution."
+        Restart-Computer -Force
+    }
+}
+
+# Function that manages the reboot counter in the registry
 function Manage-RebootCounter {
     if (-not (Test-Path -Path $RebootCountRegistryPath)) {
         New-Item -Path $RebootCountRegistryPath -Force | Out-Null
@@ -41,44 +64,46 @@ function Manage-RebootCounter {
     }
 }
 
-$RebootCount = Manage-RebootCounter
-
-if ($RebootCount -ge $MaxReboots) {
-    Log-Message "Maximum reboot limit ($MaxReboots) reached."
-    Cleanup-Script
-}
-
+# Function that syncs system time with Windows time servers to ensure that Windows Updates can be downloaded
 function Sync-SystemTime {
     Log-Message "Syncing system time with Windows time servers."
     try {
-        $W32tmService = Get-Service -Name "w32time" -ErrorAction SilentlyContinue
-        if ($W32tmService.Status -ne "Running") {
-            Log-Message "Starting w32time service."
-            Start-Service -Name "w32time"
-        }
-
-        w32tm /config /manualpeerlist:"time.windows.com" /syncfromflags:manual /reliable:yes /update
-        w32tm /resync
+        net stop w32time
+        w32tm /unregister
+        w32tm /register
+        net start w32time
+        w32tm /resync /nowait
         Log-Message "System time synced successfully."
-
-        Log-Message "Restarting w32time service."
-        Restart-Service -Name "w32time"
-        Log-Message "w32time service restarted successfully."
     } catch {
         Log-Message "Failed to sync system time: $_" -Level "ERROR"
     }
 }
 
+# Function that installs NuGet Package Provider that is required for PSWindowsUpdate module
 function Install-NuGetPackageProvider {
     Log-Message "Attempting to install NuGet Package Provider."
     try {
         Install-PackageProvider -Name NuGet -Force -Scope AllUsers
+        # Check nuget package provider is present in the list of package providers
+        $nugetProvider = Get-PackageProvider -ListAvailable | Where-Object { $_.Name -eq "NuGet" }
+        if (-not $nugetProvider) {
+            Log-Message "NuGet Package Provider not found in the list of package providers." -Level "ERROR"
+            Read-Host "Voulez-vous receduler le script au prochain redemarrage ? (O/N)" -OutVariable response
+            if ($response -eq "O") {
+                Reschedule-Script -ForceReboot $false
+                Remove-Item -Path $RebootCountRegistryPath -Recurse -Force
+                Exit
+            } else {
+                Cleanup-Script -GenerateLog $false
+            }
+        }
         Log-Message "NuGet Package Provider installed successfully."
     } catch {
         Log-Message "Failed to install NuGet Package Provider: $_" -Level "ERROR"
     }
 }
 
+# Function that installs PSWindowsUpdate module to manage Windows Updates
 function Install-PSWindowsUpdateModule {
     Log-Message "Checking for PSWindowsUpdate module."
     if (-not (Get-Module -ListAvailable -Name PSWindowsUpdate)) {
@@ -94,6 +119,7 @@ function Install-PSWindowsUpdateModule {
     }
 }
 
+# Function that imports PSWindowsUpdate module to use its cmdlets
 function Import-PSWindowsUpdateModule {
     Log-Message "Importing PSWindowsUpdate module."
     try {
@@ -104,14 +130,20 @@ function Import-PSWindowsUpdateModule {
     }
 }
 
-function Reschedule-Script {
-    Log-Message "Rescheduling script to run after reboot."
-    Set-ItemProperty -Path $RunOnceRegistryPath -Name "AutomateUpdates" -Value "Powershell.exe -ExecutionPolicy Bypass -File `"$ScriptPath`""
-    Log-Message "Rebooting the system to continue script execution."
-    Restart-Computer -Force
+# --- Main script logic --
+
+# Check if maximum reboot limit has been reached. If so, cleanup the script and exit.
+# This is to prevent the script from running indefinitely in case an update would keep reappearing after reboot.
+$RebootCount = Manage-RebootCounter
+
+# Check if maximum reboot limit has been reached. If so, cleanup the script and exit.
+if ($RebootCount -ge $MaxReboots) {
+    Log-Message "Maximum reboot limit ($MaxReboots) reached."
+    Cleanup-Script
 }
 
 # Ensure system time is synced and required modules are installed before running Windows Update
+# This step is only performed on the first run of the script
 if ($RebootCount -eq 0) {
     Sync-SystemTime
     Install-NuGetPackageProvider
@@ -119,6 +151,7 @@ if ($RebootCount -eq 0) {
     Import-PSWindowsUpdateModule
 }
 
+# Run Windows Update and handle any errors that occur
 try {
     log-message "Starting Windows Update."
     $Updates = Get-WindowsUpdate -AcceptAll -Install -AutoReboot
